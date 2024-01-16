665189d56915c8ce03f717473ea832b656d9fd18
Calebe Oliveira <calebe81899026@gmail.com>
2024-01-16 16:49:00 -0300

    Add Product
    Added static product

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,21 @@
 import Image from 'next/image'
 
 export default function Home() {
+
+  const product = {
+    name: 'Logitech MX Master 3s',
+    available: true,
+    totalPrice: 460.90,
+    basePrice: 719.90,
+    description: 'Esse produto é top, mano. Confia!',
+    imageUrls: [
+      "https://resource.logitech.com/content/dam/logitech/en/products/mice/mx-master-3s/gallery/mx-master-3s-mouse-top-view-pale-gray.png",
+      "https://resource.logitech.com/w_692,c_lpad,ar_4:3,q_auto,f_auto,dpr_2.0/d_transparent.gif/content/dam/logitech/en/products/mice/mx-master-3s/gallery/mx-master-3s-mouse-back-view-pale-gray.png?v=1",
+      "https://resource.logitech.com/w_692,c_lpad,ar_4:3,q_auto,f_auto,dpr_2.0/d_transparent.gif/content/dam/logitech/en/products/mice/mx-master-3s/gallery/mx-master-3s-mouse-3-4-view-pale-gray.png?v=1",
+      "https://resource.logitech.com/w_692,c_lpad,ar_4:3,q_auto,f_auto,dpr_2.0/d_transparent.gif/content/dam/logitech/en/products/mice/mx-master-3s/gallery/mx-master-3s-mouse-front-view-pale-gray.png?v=1",
+    ],
+  }
+
   return (
     <div>
       
